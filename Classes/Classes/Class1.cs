365e5e69c458f9e09a79ba87f5672c9fbdbab318
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-06-17 09:45:17 +0200

    Add files via upload

---
+++ Classes/Classes/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+   
+
+    internal class Character
+    {
+        public string name;
+        public int pv;
+        public int att;
+        public int def;
+
+
+        public Character(string name, int pdv, int attaque, int defense)
+        {
+            this.name = name;
+            this.pv = pdv;
+            this.att = attaque;
+            this.def = defense; 
+        }
+
+        public void Attack(Character attaque)
+        {
+            attaque.pv -= (this.att - attaque.def);
+            
+        }
+
+        public bool IsAlive()
+        {
+            if (this.pv <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+
+    
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using Classes;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Character perso1 = new Character("toto", 40, 20, 30);
+        Character perso2 = new Character("titi", 50, 100, 15);
+
+        perso1.Attack(perso2);
+        perso2.Attack(perso1);
+
+        Console.WriteLine(" Pv perso 1 : " + perso1.pv);
+        Console.WriteLine(" Pv perso 2 : " + perso2.pv);
+
+        Console.WriteLine($"Le personnage 1 est toujours en vie : {perso1.IsAlive()}");
+    }
+}
